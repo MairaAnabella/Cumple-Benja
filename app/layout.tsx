7a31540f0af8799ef73478126da8e5b90391900d
Maira Anabella <anabellamaira3@gmail.com>
2025-11-06 13:43:29 -0300

    actualizacion

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'FELIZ CUMPLEAÑOS NIÑO',
+  description: 'Te quiero ',
   generator: 'v0.app',
 }
 
